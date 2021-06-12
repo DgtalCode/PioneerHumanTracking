58caa864ef1236b6bfbb2d0fe1a7ef9f372153ed
Dgtal1 <enderdan80@gmail.com>
2021-06-12 03:00:19 +0300

    Poses are detecting

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import cv2
 import time
+import mediapipe as mp
 import numpy as np
-#from pioneer_sdk import Pioneer
 from piosdk import Pioneer
 
-useIntegratedCam = False
+useIntegratedCam = True
 
 if not useIntegratedCam:
     pioneer = Pioneer()
+else:
+    cap = cv2.VideoCapture(0)
+
+mpDrawings = mp.solutions.drawing_utils
+skeletonDetectorConfigurator = mp.solutions.pose
+skDetector = skeletonDetectorConfigurator.Pose(static_image_mode=False,
+                                               min_tracking_confidence=0.7,
+                                               min_detection_confidence=0.6, model_complexity=2)
 
 cordX = .0
 cordY = .0
@@ -35,18 +43,12 @@ y_kd = .0001
 manually = True
 flying = False
 
-heightMode = True
-
-inWidth = 368
-inHeight = 368
-threshold = 0.3
-
 last_seen = None
 
 takePhoto = False
 takePhotoTime = 0
 
-nPoints = 18
+nPoints = 32
 JOINTS_LIST = {"neck": [1, 0],
                "right_clavicle": [1, 2],
                "right_arm": [2, 3],
@@ -76,13 +78,7 @@ def sign(num):
 
 def vec(name):
     """ создает вектор на основе задетекченных точек по переданному имени """
-    try:
-        (a, b) = JOINTS_LIST[name]
-        v = np.array([points[b][0] - points[a][0],
-                      -(points[b][1] - points[a][1])])
-        return v
-    except:
-        return None
+    pass
 
 
 def angle(vec_1, vec_2):
@@ -99,271 +95,50 @@ def near(real_value, value, accuracy=15):
     """ Считает, равны ли примерно переданные значения """
     if real_value is None:
         return False
-    if real_value >= value - accuracy and real_value <= value + accuracy:
+    if value - accuracy <= real_value <= value + accuracy:
         return True
 
 
 def not_none(*args):
-    """ приверят, не является ли неизвестным хотя бы один из переданных аргументов """
+    """ проверят, не является ли неизвестным хотя бы один из переданных аргументов """
     for arg in args:
         if arg is None:
             return False
     return True
 
 
-protoFile = "pose_deploy_linevec.prototxt"
-weightsFile = "pose_iter_440000.caffemodel"
+'''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ГЛАВНЫЙ ЦИКЛ~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
 
-if useIntegratedCam:
-    cap = cv2.VideoCapture(0)
-
-net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
-
-DEVICE = "gpu"
-
-if DEVICE == "cpu":
-    net.setPreferableBackend(cv2.dnn.DNN_TARGET_CPU)
-    print("Using CPU device")
-elif DEVICE == "gpu":
-    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
-    print("Using GPU device")
-
-np.set_printoptions(threshold=np.inf)
-
-old_time = 0
-
-newCommand = False
+frame = None
 
 while True:
-    #print()
-
-    t = time.time()
+    # считывание кадра либо с веб-камеры, либо с пионера
     if useIntegratedCam:
-        # получение изображения с вебкамеры
-        hasFrame, frame = cap.read()
+        ret, frame = cap.read()
+        if not ret:
+            continue
     else:
-        # получение изображения с коптера
-        frame = cv2.imdecode(np.frombuffer(pioneer.get_raw_video_frame(), dtype=np.uint8), cv2.IMREAD_COLOR)
+        img = pioneer.get_raw_video_frame()
+        frame = cv2.imdecode(np.frombuffer(img, dtype=np.uint8), cv2.IMREAD_COLOR)
 
-    frameCopy = np.copy(frame)
+    IMG_WIDTH = np.shape(frame)[1]
+    IMG_HEIGHT = np.shape(frame)[0]
 
-    '''~~~~~~~~~~~~~~~~~~~~~~~~~~~ ФОТОГРАФИЯ БЕЗ НАДПИСЕЙ И ПРОЧЕГО ~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
-    if takePhoto and time.time() - takePhotoTime > 3:
-        cv2.imwrite('./imgae.jpg', frame)
-        print("Photo taken at {}".format(time.ctime()))
-        takePhoto = False
-        if not useIntegratedCam:
-            pioneer.led_control(255, 0, 0, 0)
-    '''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
+    frame.flags.writeable = False
+    detected_skeletons = skDetector.process(frame)
+    frame.flags.writeable = True
 
-    frameWidth = frame.shape[1]
-    frameHeight = frame.shape[0]
+    if detected_skeletons.pose_landmarks is not None:
+        points = detected_skeletons.pose_landmarks.landmark
 
-    # конвертим входное изображение под необходимые для нейросети размеры
-    inpBlob = cv2.dnn.blobFromImage(frame, 1.0 / 255, (inWidth, inHeight),
-                                    (0, 0, 0), swapRB=False, crop=False)
-    net.setInput(inpBlob)
-    output = net.forward()
+    mpDrawings.draw_landmarks(frame, detected_skeletons.pose_landmarks,
+                              skeletonDetectorConfigurator.POSE_CONNECTIONS)
 
-    H = output.shape[2]
-    W = output.shape[3]
-    # лист для хранения найденных кейпоинтов
-    points = []
+    cv2.imshow("frame", frame)
 
-    for i in range(nPoints):
-        # карта уверенности нейросети
-        probMap = output[0, i, :, :]
-
-        # поиск максимумов на карте
-        minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
-
-        # преобразование координат с выхода нейросети к изначальной системе
-        x = (frameWidth * point[0]) / W
-        y = (frameHeight * point[1]) / H
-
-        if prob > threshold:
-            cv2.circle(frameCopy, (int(x), int(y)), 8, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
-            cv2.putText(frameCopy, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2,
-                        lineType=cv2.LINE_AA)
-
-            # добавляем кейпоинт в массив, если уверенность в нем больше определенного порога
-            points.append((int(x), int(y)))
-        else:
-            points.append(None)
-
-    '''~~~~~~~~~~~~~~~~~~~~~~~~~~~ ОТРИСОВКА СКЕЛЕТА ~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
-    for pair in JOINTS_LIST.values():
-        partA = pair[0]
-        partB = pair[1]
-
-        if points[partA] and points[partB]:
-            cv2.line(frame, points[partA], points[partB], (0, 255, 255), 3, lineType=cv2.LINE_AA)
-            cv2.circle(frame, points[partA], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
-            cv2.circle(frame, points[partB], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
-
-    cv2.putText(frame, "time taken = {:.2f} sec".format(time.time() - t), (50, 50), cv2.FONT_HERSHEY_COMPLEX, .8,
-                (255, 50, 0), 2, lineType=cv2.LINE_AA)
-
-    '''~~~~~~~~~~~~~~~~~~~~~~~~~~~ ПРОВЕРКА ЖЕСТОВ ~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
-    # создание векторов, описывающих части тела
-    right_forearm = vec("left_forearm")
-    right_arm = vec("left_arm")
-    left_forearm = vec("right_forearm")
-    left_arm = vec("right_arm")
-
-    # поиск углов в сочленениях
-    right_elbow_alpha = angle(right_arm, right_forearm)
-    right_arm_alpha = angle(right_arm, [100, 0])
-    left_elbow_alpha = angle(left_arm, left_forearm)
-    left_arm_alpha = angle(left_arm, [-100, 0])
-
-    # массив для хранения распознанных действий
-    # (необходим, чтобы не происходило детекта больше, чем одного жеста)
-    action = []
-
-    # выводим на экран все углы
-    cv2.putText(frame, str(right_arm_alpha), (450, 50), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 50, 255), 2,
-                lineType=cv2.LINE_AA)
-    cv2.putText(frame, str(right_elbow_alpha), (450, 150), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 50, 255), 2,
-                lineType=cv2.LINE_AA)
-    cv2.putText(frame, str(left_arm_alpha), (450, 250), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 50, 255), 2,
-                lineType=cv2.LINE_AA)
-    cv2.putText(frame, str(left_elbow_alpha), (450, 350), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 50, 255), 2,
-                lineType=cv2.LINE_AA)
-
-    if points[2] is not None and points[5] is not None:
-        cv2.putText(frame, str(abs(points[2][0] - points[5][0])), (150, 350), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 50, 255), 2, lineType=cv2.LINE_AA)
-
-    if near(right_arm_alpha, 0) and near(right_elbow_alpha, -90) and right_forearm[1] > 0:
-        action.append(1)
-
-    if near(right_arm_alpha, 0) and near(right_elbow_alpha, -15):
-        action.append(2)
-
-    if near(left_arm_alpha, 0) and near(left_elbow_alpha, 90) and left_forearm[1] > 0:
-        action.append(3)
-
-    if near(left_arm_alpha, 0) and near(left_elbow_alpha, 15):
-        action.append(4)
-
-    if near(left_arm_alpha, 80, 20) and near(right_arm_alpha, -80, 20) and \
-            near(left_elbow_alpha, 145, 20) and near(right_elbow_alpha, -145, 20) and \
-            right_arm[0] > left_arm[0]:
-        action.append(5)
-
-    if near(left_arm_alpha, 0, 20) and near(right_arm_alpha, 0, 20) and \
-            near(left_elbow_alpha, 90, 20) and near(right_elbow_alpha, -90, 20) and \
-            right_forearm[1] < 0 and left_forearm[1] < 0:
-        action.append(6)
-
-    # если распознан только один жест, то выводим его на экран
-    # иначе говорим. что ничего не найдено
-    if len(action) == 1:
-        cv2.putText(frame, "G %i" % action[0], (50, 150), cv2.FONT_HERSHEY_COMPLEX, 3, (0, 50, 255), 2,
-                    lineType=cv2.LINE_AA)
-    else:
-        cv2.putText(frame, "G %i" % (-1), (50, 150), cv2.FONT_HERSHEY_COMPLEX, 3, (0, 50, 255), 2, lineType=cv2.LINE_AA)
-
-    cv2.imshow('kek', frameCopy)
-    cv2.imshow('Output-Skeleton', frame)
-
-    '''~~~~~~~~~~~~~~~~~~~~~~~~~~~ УПРАВЛЕНИЕ КВАДРОКОПТЕРОМ ~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
-    print(pioneer.get_task_id())
-    if pioneer.get_task_id() == 0:
-        key = cv2.waitKey(1)
-        #newCommand = False
-
-        if points[1] is not None:
-            last_seen = points[1]
-
-        # space
-        if key == 32:
-            pioneer.arm()
-            pioneer.takeoff()
-            flying = True
-        # esc
-        elif key == 27:
-            pioneer.land()
-            flying = False
-        elif key == ord('k') and not flying:
-            break
-        elif key == ord('m'):
-            manually = not manually
-
-        if time.time() - old_time > 0.1:
-            if not newCommand:
-                if key == ord('w') or (len(action) == 1 and action[0] == 1):
-                    cordY += speedXYZ
-                    newCommand = True
-                elif key == ord('s') or (len(action) == 1 and action[0] == 3):
-                    cordY -= speedXYZ
-                    newCommand = True
-                if key == ord('d') or (len(action) == 1 and action[0] == 2):
-                    cordX += speedXYZ
-                    newCommand = True
-                elif key == ord('a') or (len(action) == 1 and action[0] == 4):
-                    cordX -= speedXYZ
-                    newCommand = True
-
-                if key == ord('r'):
-                    cordZ += speedXYZ
-                    newCommand = True
-                elif key == ord('f'):
-                    cordZ -= speedXYZ
-                    newCommand = True
-
-                if key == ord('q'):
-                    yaw += speedYaw
-                    newCommand = True
-                elif key == ord('e'):
-                    yaw -= speedYaw
-                    newCommand = True
-
-                if len(action) == 1 and action[0] == 5 and not takePhoto:
-                    takePhoto = True
-                    takePhotoTime = time.time()
-                    print("Be ready!")
-                    if not useIntegratedCam:
-                        pioneer.led_control(255, 255, 0, 0)
-
-                if len(action) == 1 and action[0] == 6:
-                    pioneer.land();
-                    flying = False
-
-            if manually and flying and newCommand:
-                old_time = time.time()
-                if not useIntegratedCam:
-                    pioneer.go_to_local_point(x=cordX, y=cordY, z=cordZ, yaw=yaw)
-                newCommand = False
-
-            elif not manually and last_seen is not None:
-                newCommand = False
-                old_time = time.time()
-                v1 = [0, -100]
-                v2 = [frameWidth // 2 - last_seen[0], -last_seen[1]]
-
-                yaw_err = frameWidth//2 - last_seen[0]
-                yaw += yaw_kp * yaw_err + yaw_kd * (yaw_err - yaw_errold)
-                yaw_errold = yaw_err
-
-                if heightMode:
-                    z_err = frameHeight//2 - last_seen[1]
-                    cordZ += z_kp * z_err + z_kd * (z_err - z_errold)
-                    z_errold = z_err
-                else:
-                    y_err = frameHeight//2 - last_seen[1]
-                    cordY = y_kp * y_err + y_kd * (y_err - y_errold)
-                    y_errold = y_err
-
-                pioneer.go_to_local_point(x=cordX, y=cordY, z=cordZ, yaw=np.radians(yaw))
-    elif pioneer.get_task_id() == 1:
-        pioneer.takeoff()
-    elif pioneer.get_task_id() == 2:
-        pioneer.land()
-    elif pioneer.get_task_id() == 3:
-        pioneer.go_to_local_point_as_loop()
-
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
