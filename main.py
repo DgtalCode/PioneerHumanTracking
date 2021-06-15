e2ec9d0d65b4b8e61c8e09591e47421791884a8c
Dgtal1 <enderdan80@gmail.com>
2021-06-16 02:03:59 +0300

    Preparation for NN using

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import time
 import mediapipe as mp
 import numpy as np
 from piosdk import Pioneer
+from collections import namedtuple
+
+Point = namedtuple("Point", "x y visibility")
 
 useIntegratedCam = True
 
@@ -14,8 +17,8 @@ else:
 mpDrawings = mp.solutions.drawing_utils
 skeletonDetectorConfigurator = mp.solutions.pose
 skDetector = skeletonDetectorConfigurator.Pose(static_image_mode=False,
-                                               min_tracking_confidence=0.7,
-                                               min_detection_confidence=0.6, model_complexity=2)
+                                               min_tracking_confidence=0.5,
+                                               min_detection_confidence=0.5, model_complexity=2)
 
 cordX = .0
 cordY = .0
@@ -130,6 +133,19 @@ while True:
 
     if detected_skeletons.pose_landmarks is not None:
         points = detected_skeletons.pose_landmarks.landmark
+        relative_points = []
+
+        base_point = Point((points[12].x + points[11].x)/2,
+                           (points[12].y + points[11].y)/2,
+                           (points[12].visibility + points[11].visibility)/2)
+
+        for point in points:
+            relative_point = Point(point.x - base_point.x, point.y - base_point.y, point.visibility)
+            relative_points.append(relative_point)
+
+        x = round(IMG_WIDTH * base_point[0])
+        y = round(IMG_HEIGHT * base_point[1])
+        cv2.circle(frame, (x, y), 3, (255,255,0), 2)
 
     mpDrawings.draw_landmarks(frame, detected_skeletons.pose_landmarks,
                               skeletonDetectorConfigurator.POSE_CONNECTIONS)
@@ -142,3 +158,4 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
