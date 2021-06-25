a4e64ecbf152ff9ad5ea41139afe9ce5fe166431
Dgtal1 <enderdan80@gmail.com>
2021-06-26 00:35:54 +0300

    Working angles calculation

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ import mediapipe as mp
 import numpy as np
 from piosdk import Pioneer
 from SkeletonPosePredictor import VecPosePredictor
+import SkeletonPosePredictor as sk
 import time
-
-# predictor = PosePredictor()
-predictor = VecPosePredictor('./data.json')
 
 useIntegratedCam = True
 
 if not useIntegratedCam:
     pioneer = Pioneer()
+    img_shape = (640, 480)
 else:
     cap = cv2.VideoCapture(0)
+    img_shape = (1920, 1080)
+
+# predictor = VecPosePredictor('./data.json')
 
 mpDrawings = mp.solutions.drawing_utils
 skeletonDetectorConfigurator = mp.solutions.pose
@@ -54,15 +56,15 @@ last_seen = None
 takePhoto = False
 takePhotoTime = 0
 
-nPoints = 32
-JOINTS_LIST = {"neck": [None, 0],
-               "right_clavicle": [None, 12],
-               "right_arm": [12, 14],
-               "right_forearm": [14, 16],
-               "left_clavicle": [None, 11],
-               "left_arm": [11, 13],
-               "left_forearm": [13, 15]}
+JOINTS_LIST = {"neck": [33, 0],
+               "left_clavicle": [33, 12],
+               "left_arm": [12, 14],
+               "left_forearm": [14, 16],
+               "right_clavicle": [33, 11],
+               "right_arm": [11, 13],
+               "right_forearm": [13, 15]}
 
+predictor = VecPosePredictor(JOINTS_LIST)
 
 '''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ГЛАВНЫЙ ЦИКЛ~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
 
@@ -93,11 +95,20 @@ while True:
     if detected_skeletons.pose_landmarks is not None:
         points = detected_skeletons.pose_landmarks.landmark
 
-        relative_points = predictor.calculate_relative_points(points)
-        # print(type(relative_points))
+        base_point = sk.Point(x=(points[12].x + points[11].x) / 2,
+                              y=(points[12].y + points[11].y) / 2,
+                              z=(points[12].z + points[11].z) / 2,
+                              visibility=(points[12].visibility + points[11].visibility) / 2)
 
-        # print(relative_points)
-        # print(np.array(relative_points, dtype=np.double).flatten())
+        converted_points = predictor.convert_points(points)
+
+        joints = predictor.generate_joints_vectors(converted_points)
+
+        ra1 = predictor.angle_between(joints.right_clavicle, joints.right_arm)
+        ra2 = predictor.angle_between(joints.right_arm, joints.right_forearm)
+        la1 = predictor.angle_between(joints.left_clavicle, joints.left_arm)
+        la2 = predictor.angle_between(joints.left_arm, joints.left_forearm)
+        print(base_point.z)
 
         if key == ord('0'):
             predictor.start_writing(0)
@@ -107,15 +118,12 @@ while True:
             predictor.start_writing(2)
         if key == ord('x'):
             predictor.save_data()
-        predictor.update_data(relative_points)
-        if key == ord('9'):
-            # print(relative_points)
-            # print(len(relative_points))
-            predict = not predict
-        if predict and time.time() - predict_time > 1:
-            a = predictor.predict([relative_points])
-            print(a)
-            predict_time = time.time()
+        # if key == ord('9'):
+        #     predict = not predict
+        # if predict and time.time() - predict_time > 1:
+        #     a = predictor.predict([relative_points])
+        #     print(a)
+        #     predict_time = time.time()
 
     mpDrawings.draw_landmarks(frame, detected_skeletons.pose_landmarks,
                               skeletonDetectorConfigurator.POSE_CONNECTIONS)
